29fd20cb5ef70319365e027408a4fa90c779db3f
Erik Thuning <boooink@gmail.com>
2017-02-16 18:39:38 +0100

    Full functionality achieved

--- collide.py
+++ collide.py
@@ -1,40 +1,53 @@
 #!/usr/bin/env python3
 
-import fileinput
+import binascii, string, random, fileinput, re
 
-import binascii
 def hash(msg):
     return binascii.crc_hqx(msg.encode('ascii'), 0)
 
-import string, random
-def collide(hash_sum):
-    selections = string.ascii_letters + string.digits
-    length = 32
+def collide(hash_sum, sel, l):
     max_iters = 1000000
-    i = 0
 
     found = False
     for i in range(max_iters):
-        cand = ''.join(random.choice(selections) for _ in range(length))
+        cand = ''.join(random.choice(sel) for _ in range(l))
         
         if hash(cand) == hash_sum:
             found = cand
             break
                 
     return found
-    
+
+def create_package(msg, sel, l):
+    result = collide(hash(msg), sel, l)
+    if result:
+        print("[{}],{}".format(result, msg))
+        exit(0)
+    else:
+        print("No collision found.")
+        exit(1)
+
+def verify_package(msg, col):
+    if hash(msg) == hash(col):
+        print("OK.")
+        exit(0)
+    else:
+        print("Invalid.")
+        exit(1)
+
 message=""
 for line in fileinput.input():
     message = message + line
 
 message = message.strip()
-hsum = hash(message)
-result = collide(hsum)
+selections = string.ascii_letters + string.digits
+length = 32
 
-if result:
-    print("Collider: {}".format(result))
-    exit(0)
+regex = re.compile(r'\[([' + selections + r']{' + str(length) + r'})\],(.*)', re.DOTALL)
+match = regex.match(message)
+
+if match:
+    verify_package(match.group(1), match.group(2))
 else:
-    print("No collision found.")
-    exit(1)
-
+    create_package(message, selections, length)
+    
